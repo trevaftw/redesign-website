00035997e4022fa8e75e055629c58e78af0d02c1
Trevor Ramlow <trevorramlow@gmail.com>
2019-12-11 14:36:10 -0600

    header/nav bar setup with basic styling. now moving on to dropdown sidebar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="nav-bar">
-     <center>Trevor</center>
-    </div>
-  );
+//styling
+import './header.css'
+import Navicon from './navicon.svg';
+
+class App extends Component {
+
+    handleNav = () => {
+        console.log('nav clicked')
+    }
+    render() {
+        return (
+            <nav className="nav-bar">
+                <img id="nav-icon" onClick={this.handleNav} src={Navicon} alt="navigation dropdown" /><div id="title">Trevor</div><div id="hidden"></div>
+            </nav>
+        );
+    }
 }
 
 export default App;
